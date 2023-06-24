eabf19e225140d6903eae8deee87014fb06cf09d
SubaashGovindharaju <g.subaash04072002@gmail.com>
2023-06-24 17:34:11 +0530

    done this

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './card';
+
+function App() {
+  return (
+    <div className="App">
+    <Card />
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    // <div class="container-fluid">
+    //   <div class="row">
+
+    //     <div class="col-xl-4">
+    //         <div class="card">
+    //           <div class="card-body">
+    //             <h5 class="card-title">Card title</h5>
+    //             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    //           </div>
+    //           <div class="card-footer">
+    //             <small class="text-muted">Last updated 3 mins ago</small>
+    //           </div>
+    //         </div>
+    //       </div>
+
+    //     <div class="col-xl-4">
+    //       <div class="card">
+    //         <div class="card-body">
+    //           <h5 class="card-title">Card title</h5>
+    //           <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    //         </div>
+    //         <div class="card-footer">
+    //           <small class="text-muted">Last updated 3 mins ago</small>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //     <div class="col-xl-4">
+
+    //       <div class="card">
+    //         <div class="card-body">
+    //           <h5 class="card-title">Card title</h5>
+    //           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    //         </div>
+    //         <div class="card-footer">
+    //           <small class="text-muted">Last updated 3 mins ago</small>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //   </div>
+    // </div>
+
+  );
+}
+
+export default App;
